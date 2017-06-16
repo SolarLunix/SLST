9b49a64d52d7b5df41e6a3c3dbdccf0a95339125
SolarLunix <solarlunix@gmail.com>
2017-06-16 15:49:10 +0100

    Created concat
    Copied the concat method from the SLST paper to see if it will work.

--- Concatenator.py
+++ Concatenator.py
@@ -4,3 +4,18 @@
 # Description: Putting the genomic sequence back together from the fragments.
 
 
+def concat(fragments):
+    out = {}
+    count = 0
+    for frag in fragments:
+        for f in frag:
+            try:
+                out[f] += frag[f]
+            except KeyError:
+                out[f] = count*'-' + frag[f]
+        count = max([len(out[i]) for i in out])
+        for n in out:
+            while len(out[n]) < count:
+                out[n] += '-'
+
+    return out
