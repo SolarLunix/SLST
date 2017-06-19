89288e3c7890ec086abe8f88127592d177778576
SolarLunix <solarlunix@gmail.com>
2017-06-19 14:54:46 +0100

    Created understandable variables

--- Concatenator.py
+++ Concatenator.py
@@ -1,6 +1,6 @@
 # Created by: Melissa Melaugh
 # Date Created: 16 June 2017
-# Date Edited: 18 June 2017
+# Date Edited: 19 June 2017
 # Description: Putting the genomic sequence back together from the fragments.
 
 
@@ -9,17 +9,17 @@ def concat(fragments):
     count = 0
     for frag in fragments:
         # for each dictionary passed in look at the keys in the dictionary
-        for f in frag:
+        for strain in frag:
             # for each key in the dictionary take the value for the key and add it to the same key in the output
             try:
-                out[f] += frag[f]
+                out[strain] += frag[strain]
             except KeyError:
                 # if the key doesn't exist, create it.
-                out[f] = count*'-' + frag[f]
+                out[strain] = count*'-' + frag[strain]
         # make sure all the fragments line up to be the same size and add - where they don't.
         count = max([len(out[i]) for i in out])
-        for n in out:
-            while len(out[n]) < count:
-                out[n] += '-'
+        for strain in out:
+            while len(out[strain]) < count:
+                out[strain] += '-'
 
     return out
